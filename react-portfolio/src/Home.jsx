193f44aa6a4996d427c1cfcbde24a31b11914ca7
Harry <harry@macbookpro>
2024-03-06 20:20:57 +0000

    section 1 complete

--- react-portfolio/src/Home.jsx
+++ react-portfolio/src/Home.jsx
@@ -1,13 +1,27 @@
 import "./App.css";
 import logo from "./assets/github.svg";
+import computer from "./assets/computer.svg";
 export default function Home() {
   return (
     <div className="first-section">
-      <header className="header">
-        <div className="logo">
-          <img src={logo} alt="" className="logo-image" />
+      <div className="logo">
+        <img src={logo} alt="" className="logo-image" />
+      </div>
+      <div className="heading">
+        <img src={computer} alt="" className="code-image" />
+        <h1 className="name">NIDHI SHARMA</h1>
+        <div className="wrapper">
+          <div className="static-txt"> I am</div>
+          <ul className="dynamic-txts">
+            <li className="degination">
+              <span>FRONTEND DEVELOPER</span>
+            </li>
+            <li className="degination">
+              <span>Mobile Application DEVELOPER</span>
+            </li>
+          </ul>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
