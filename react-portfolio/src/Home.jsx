e06e74b2a4137584c540abe0fc3dc6695ed19e2d
Harry <harry@macbookpro>
2024-03-06 13:19:25 +0000

    added logo

--- react-portfolio/src/Home.jsx
+++ react-portfolio/src/Home.jsx
@@ -1,3 +1,13 @@
+import "./App.css";
+import logo from "./assets/github.svg";
 export default function Home() {
-  return <div className="first-section">Home</div>;
+  return (
+    <div className="first-section">
+      <header className="header">
+        <div className="logo">
+          <img src={logo} alt="" className="logo-image" />
+        </div>
+      </header>
+    </div>
+  );
 }
