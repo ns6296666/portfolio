b426c0caf6a6eca1d5785a4c62525445bf9f9ae5
Harry <harry@macbookpro>
2024-03-07 09:43:27 +0000

    project section complete

--- react-portfolio/src/Project.jsx
+++ react-portfolio/src/Project.jsx
@@ -8,60 +8,57 @@ export default function Project() {
     <div className="project">
       <Heading name="PROJECTS" />
       <div className="project-cards">
-        <Link to="https://ns6296666.github.io/dailyplanner/">
-          <div
-            className="card project-card"
-            style={{ width: "20rem", height: "350px" }}
-          >
+        <div
+          className="card project-card"
+          style={{ width: "20rem", height: "350px" }}
+        >
+          <Link to="https://ns6296666.github.io/dailyplanner/">
             <img src={planner} className="card-img-top" alt="planner" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card&apos;s content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
+          </Link>
+          <div className="card-body">
+            <span>2024</span>
+            <p className="card-title">
+              <Link
+                to="https://ns6296666.github.io/dailyplanner/"
+                className="text-center"
+              >
+                Daily Planner
+              </Link>
+            </p>
           </div>
-        </Link>
-        <Link to="https://ns6296666.github.io/WeatherDashboard/">
-          <div
-            className="card project-card"
-            style={{ width: "20rem", height: "350px" }}
-          >
+        </div>
+
+        <div
+          className="card project-card"
+          style={{ width: "20rem", height: "350px" }}
+        >
+          <Link to="https://ns6296666.github.io/WeatherDashboard/">
             <img src={forecast} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card&apos;s content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
+          </Link>
+          <div className="card-body">
+            <span>2024</span>
+            <p className="card-title">
+              <Link to="https://ns6296666.github.io/WeatherDashboard/">
+                Weather Forecast
+              </Link>
+            </p>
           </div>
-        </Link>
-        <Link to="https://www.ssdntech.com/">
-          <div
-            className="card project-card"
-            style={{ width: "20rem", height: "350px" }}
-          >
+        </div>
+
+        <div
+          className="card project-card"
+          style={{ width: "20rem", height: "350px" }}
+        >
+          <Link to="https://www.ssdntech.com/">
             <img src={ssdn} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card&apos;s content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
+          </Link>
+          <div className="card-body">
+            <span>2023</span>
+            <p className="card-title">
+              <Link to="https://www.ssdntech.com/">E-Commerce</Link>
+            </p>
           </div>
-        </Link>
+        </div>
       </div>
     </div>
   );
