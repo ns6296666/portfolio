9a652c6cf50b313ed3a82d05ef459dab496116da
Harry <harry@macbookpro>
2024-03-07 21:03:38 +0000

    created contact section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import About from "./About";
 import Project from "./Project";
 import Skills from "./Skills";
 import Experience from "./Experience";
+import Contact from "./Contact";
 import Footer from "./Footer";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Project />
       <Skills />
       <Experience />
+      <Contact />
       <Footer />
     </>
   );
--- react-portfolio/src/Footer.jsx
+++ react-portfolio/src/Footer.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 import git from "./assets/footer.svg";
+import linkedin from "./assets/linkedin.svg";
 export default function Footer() {
   return (
     <div className="footer">
-      <Link to="https://github.com/ns6296666">
-        <img src={git} alt="git" className="git" />
-      </Link>
+      <div className="footer-links">
+        <Link to="https://github.com/ns6296666">
+          <img src={git} alt="git" className="git" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/nidhisharmaleicester/">
+          <img src={linkedin} alt="linkedin" className="git" />
+        </Link>
+      </div>
 
       <p>Copyright @ Nidhi Sharma</p>
     </div>
