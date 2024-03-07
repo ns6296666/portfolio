8b89d0335be2cbe4b4154fe4191a344d3a96613c
Harry <harry@macbookpro>
2024-03-07 08:49:17 +0000

    linked projects

--- react-portfolio/src/Project.jsx
+++ react-portfolio/src/Project.jsx
@@ -1,47 +1,65 @@
 import Heading from "./Heading";
+import Link from "react-router-dom";
 
 export default function Project() {
   return (
     <div className="project">
       <Heading name="PROJECTS" />
-      <div className="card project-cards" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card&apos;s content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card project-cards" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card&apos;s content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card project-cards" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card&apos;s content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
+      <div className="project-cards">
+        <Link to="https://ns6296666.github.io/dailyplanner/">
+          <div
+            className="card project-card"
+            style={{ width: "20rem", height: "350px" }}
+          >
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card&apos;s content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </Link>
+        <Link to="https://ns6296666.github.io/WeatherDashboard/">
+          <div
+            className="card project-card"
+            style={{ width: "20rem", height: "350px" }}
+          >
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card&apos;s content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </Link>
+        <Link to="https://www.ssdntech.com/">
+          <div
+            className="card project-card"
+            style={{ width: "20rem", height: "350px" }}
+          >
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card&apos;s content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
