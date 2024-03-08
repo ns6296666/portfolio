e2631715fec51c8110f8674994412a41d151f2dc
Harry <harry@macbookpro>
2024-03-08 08:55:43 +0000

    added content to contact section

--- react-portfolio/src/Contact.jsx
+++ react-portfolio/src/Contact.jsx
@@ -1,3 +1,28 @@
+import call from "./assets/call.svg";
+import whatsapp from "./assets/whatsapp.svg";
 export default function Contact() {
-  return <div className="contact">Contact</div>;
+  return (
+    <div className="contact">
+      <div className="description">
+        <p className="query">For any query</p>
+        <p className="free">Please Feel free to CONTACT me</p>
+      </div>
+      <div>
+        <div className="information">
+          <p className="for-call">For Call </p>
+          <button>
+            <img src={call} alt="call" className="call" />
+            <a href="tel:7741785330"> +44 7741785330 </a>
+          </button>
+        </div>
+        <div>
+          <p className="for-call">For Email</p>
+          <button>
+            <img src={whatsapp} alt="call" className="call" />
+            <a href="mailto:ns6296666@gmail.com">ns6296666@gmail.com</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
