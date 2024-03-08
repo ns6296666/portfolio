5c1f421450221067b3ea32a1e40292593221c2be
Harry <harry@macbookpro>
2024-03-08 09:24:52 +0000

    contact right section css complete

--- react-portfolio/src/Contact.jsx
+++ react-portfolio/src/Contact.jsx
@@ -9,15 +9,15 @@ export default function Contact() {
           Please Feel free to <span>CONTACT</span> me
         </p>
       </div>
-      <div>
-        <div className="information">
+      <div className="right-section">
+        <div className="information mr-2">
           <p className="for-call">For Call </p>
           <button>
             <img src={call} alt="call" className="call" />
             <a href="tel:7741785330"> +44 7741785330 </a>
           </button>
         </div>
-        <div>
+        <div className="information">
           <p className="for-call">For Email</p>
           <button>
             <img src={whatsapp} alt="call" className="call" />
