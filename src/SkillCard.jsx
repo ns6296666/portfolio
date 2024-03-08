084a7f54412f34d7aca1b4fbe6467d2f673fb6b9
Nidhi <nidhi@macbookpro>
2024-03-08 22:13:59 +0000

    skills section mobile responsiveness done

--- src/SkillCard.jsx
+++ src/SkillCard.jsx
@@ -2,10 +2,7 @@ import "./App.css";
 
 export default function SkillCard({ image, name }) {
   return (
-    <div
-      className="card skill-card"
-      style={{ width: "100px", height: "100px" }}
-    >
+    <div className="card skill-card" style={{}}>
       <img src={image} className="skill-img" />
       <p className="skill">{name}</p>
     </div>
